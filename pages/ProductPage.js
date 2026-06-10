862add1649d9cd4f4c62086657302790913317a3
Shivangi <aapple@silicons-MacBook-Air.local>
2026-06-10 23:41:33 +0530

    hover scroll wait for element

--- pages/ProductPage.js
+++ pages/ProductPage.js
@@ -5,6 +5,15 @@ class ProductPage {
         this.page = page;
         this.productsLink = page.getByRole('link', { name: 'Products' });
         this.productTitle = page.locator('//h2[contains(text(),"All Products")]');
+        this.searchProductInput = page.locator('input[id="search_product"]');
+        this.searchProductBtn = page.locator('button[id="submit_search"]');
+        this.productList = page.locator('//div[@class="productinfo text-center"]/p');
+        this.viewProductBtn = page.locator('//div[@class="productinfo text-center"]/following::div[@class="choose"]/ul/li/a');
+        this.searchProductTiltle = page.locator('//h2[contains(text(),"Searched Products")]');
+        this.productImage = page.locator('//div[@class="product-image-wrapper"]');
+        this.overlayaddtocartBtn = page.locator('//div[@class="product-overlay"]/div//a[contains(text(),"Add to cart")]');
+        this.cartmodalpopup = page.locator('//div[@id="cartModal"]');
+        this.continueShoppingBtn = page.getByRole('button', { name: 'Continue Shopping' });
     }
 
     async clickProductsLink() {
@@ -17,15 +26,41 @@ class ProductPage {
     async validateProductsList() {
         await this.page.waitForLoadState('domcontentloaded');
         await this.productTitle.waitFor({ state: 'visible' });
-        const productList = await this.page.locator('//div[@class="productinfo text-center"]/p').allTextContents();
-        console.log(productList);
+        const forproductList = await this.productList.allTextContents();
+        console.log(forproductList);
     }
     async clickViewProduct() {
-        const viewProductbtn = await this.page.locator('//div[@class="productinfo text-center"]/following::div[@class="choose"]/ul/li/a');
-        const count = await viewProductbtn.count();
-        console.log('Number of products: ' + count);
-        await viewProductbtn.nth(0).click();
+        const forviewProductbtn = this.viewProductBtn;
+        console.log('Number of products: ' + (await forviewProductbtn.count()));
+        await forviewProductbtn.nth(0).click();
     }
+    async validateProductDetailUrl() {
+        await expect(this.page).toHaveURL(/product_details/);
+    }
+    async searchProduct(productName) {
+        await this.searchProductInput.fill(productName);
+        await this.searchProductBtn.click();
+        await this.page.waitForLoadState('domcontentloaded');
+        await this.searchProductTiltle.waitFor({ state: 'visible' });
+        const searchResults = await this.productList.allTextContents();
+        console.log('Search results for "' + productName + '":', searchResults);
+        console.log('Number of search results: ' + (await searchResults.length));
+    }
+    async clickAddToCart() {
+        await this.productImage.first().scrollIntoViewIfNeeded();
+        await this.productImage.first().hover();
+        await this.overlayaddtocartBtn.first().waitFor({ state: 'visible' });
+        await this.overlayaddtocartBtn.first().click();
+    }
+
+        async validateCartModalPopup() {
+        await expect(this.cartmodalpopup).toBeVisible();
+        await this.continueShoppingBtn.click();
+        await expect(this.cartmodalpopup).not.toBeVisible();
+
+        }
+
+
 }
 
 module.exports = ProductPage;
--- tests/product.spec.js
+++ tests/product.spec.js
@@ -5,8 +5,14 @@ test('Validate product page', async ({ productpage }) => {
     await productpage.page.goto('https://automationexercise.com', {
         waitUntil: 'domcontentloaded',
     });
+    // await productpage.clickProductsLink();
+    // await productpage.validateProductPage();
+    // await productpage.validateProductsList();
+    // await productpage.clickViewProduct();
+    // await productpage.validateProductDetailUrl();
     await productpage.clickProductsLink();
-    await productpage.validateProductPage();
-    await productpage.validateProductsList();
-    await productpage.clickViewProduct();
+    await productpage.searchProduct('Tops'); 
+    await productpage.clickAddToCart();
+    await productpage.validateCartModalPopup();
+
 });

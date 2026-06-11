043af47d6bd0643ffe861a8995c15d6e015cb434
Shivangi <aapple@silicons-MacBook-Air.local>
2026-06-11 23:38:23 +0530

    Parallel execution and added github actions CI

--- fixtures/amazonFixture.js
+++
@@ -1,9 +0,0 @@
-const base = require('@playwright/test');
-const AmazonPage = require('../pages/AmazonPage');
-
-exports.test = base.test.extend({
-  amazonPage: async ({ page }, use) => {
-    const amazonPage = new AmazonPage(page);
-    await use(amazonPage);
-  },
-});
